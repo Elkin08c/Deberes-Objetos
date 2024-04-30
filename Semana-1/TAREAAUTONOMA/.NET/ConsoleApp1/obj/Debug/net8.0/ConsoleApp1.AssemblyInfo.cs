89e7fc793ec213c0c76c0faf6d7d1ac28335164e
Elkin08c <146010530+Elkin08c@users.noreply.github.com>
2024-04-29 20:33:52 -0500

    commit

--- Semana-1/TAREAAUTONOMA/.NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Semana-1/TAREAAUTONOMA/.NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d57a2c0da9f58cc2718297e174d33c894ed062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2252eea476c8839d6abd817a1ba49e973df98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
